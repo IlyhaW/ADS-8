133217fa50a70c4e317e55d33d372c3de782ca79
IlyhaW <125214907+IlyhaW@users.noreply.github.com>
2023-05-06 09:55:40 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,58 @@
-// Copyright 2021 NNTU-CS
 #include "train.h"
+
+Train :: Train():first(nullptr), countOp(0) {}
+void Train :: addCage(bool light) {
+  Cage* items = new Cage;
+  items->next = nullptr;
+  items->prev = nullptr;
+  Cage* temp;
+  items->light = light;
+  if (first == nullptr) {
+    first = items;
+  } else {
+    if (first->next == nullptr) {
+      first->next = items;
+      first->prev = items;
+      items->prev = first;
+      items->next = first;
+    } else {
+      temp = first;
+      while (temp->next != first) {
+        temp = temp->next;
+      }
+      temp->next = items;
+      items->prev = temp;
+      items->next = first;
+      first->prev  = items;
+    }
+  }
+  countOp = 0;
+}
+int Train :: getLength() {
+  Cage* temp;
+  int sCage;
+  countOp = 0;
+  first->light = true;
+  while (first->light == true) {
+    temp = first->next;
+    while (temp->light == false) {
+      temp = temp->next;
+      countOp++;
+    }
+    countOp++;
+    temp->light = false;
+    sCage = 1;
+    while (temp->prev != first) {
+      temp = temp->prev;
+      countOp++;
+      sCage++;
+    }
+    countOp++;
+  }
+  return sCage;
+}
+int Train :: getOpCount() {
+  if (countOp == 0)
+    getLength();
+  return countOp;
+}
